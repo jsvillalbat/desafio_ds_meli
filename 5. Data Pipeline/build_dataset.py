8e6f4ec262ff877e36a1b6a15dadd39e82d1f29b
Sebastian Villalba <juan.villalba@soytul.com>
2023-03-13 16:54:22 -0500

    ADD final changes and Update README

--- 5. Data Pipeline/build_dataset.py
+++ 5. Data Pipeline/build_dataset.py
@@ -3,12 +3,14 @@ import requests
 import json
 import pandas as pd
 
-from loguru import logger
 from pydantic import BaseModel, Field
-from typing import Union, Optional
+from typing import  Optional
 
 
 class ItemResponse(BaseModel):
+    """
+    Clase que modela cada Item individual que retorna el endpoint
+    """
     id: str = Field(...)
     title: str = Field(...)
     condition: str = Field(...)
@@ -23,18 +25,24 @@ class ItemResponse(BaseModel):
 
 
 class ListItemsResponse(BaseModel):
+    """ Clase que modela toda la lista de items que retorna el endpoint
+    """
     results: list[ItemResponse] = Field(...)
     
 
 class DatasetItemsGenerator():
-
-    def __init__(self, site: str, max_items_offset: int = 5000) -> None:
+    """Clase que Genera el dataset de items
+    """
+    def __init__(self, site: str, max_items_offset: int = 1000) -> None:
         self.site: str = site
         self.init_offset: int = 0
         self.items_response: list = []
         self.max_items_offset: int  = max_items_offset
 
     def get_raw_data(self):
+        """
+        Función que obtiene la data cruda del endpoint
+        """
         while self.init_offset <= self.max_items_offset:
             url = f"https://api.mercadolibre.com/sites/{self.site}/search?q=tv%204k&offset={self.init_offset}"
             response = requests.get(url=url)
@@ -43,17 +51,25 @@ class DatasetItemsGenerator():
             self.init_offset+=50
 
     def transform_data(self):
+        """Función que transforma la data en una tabla y obtiene la marca de cada item
+        """
         items = [item.get_brand().dict(exclude={'attributes'}) for item in self.items_response]
         structured_data = pd.DataFrame.from_records(items)
         self.data = structured_data[structured_data.condition == 'new']
 
     def save_dataset(self):
+        """Función apra guardar el dataset en un archivo csv
+        """
         self.data.to_csv("dataset.csv", columns=["id", "title" ,"price", "domain_id","brand"])
 
 
 if __name__ == '__main__':
+    # Se obtiene el site (MLA, MLM, MLB)
     site = sys.argv[1]
     dataset_generator = DatasetItemsGenerator(site)
+    # Se obtiene la data cruda
     dataset_generator.get_raw_data()
+    #Se transforma la data al formato requerido 
     dataset_generator.transform_data()
+    # Se guarda la data en un archivo csv
     dataset_generator.save_dataset()
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# desafio_ds_meli
-Ejercicios resueltos del data science challenge de Mercado Libre
+# Ejercicios Desafio Meli
+## Autor: Juan Sebastian Villalba Torres
+Ejercicios resueltos del data science challenge de Mercado Libre.
+Se realizaron los ejercicio 3,4 y 5.
+
+Primero se deben instalar las dependencias y librerias de Python que se encuentran en el archivo **requirements.txt**
+Para esto es recomendable crear un entorno virtual usando conda o virtualenv
+
+```bash
+virtualenv venv
+```
+Y luego instalar las dependencias con el comando
+```bash
+pip install -r requirements.txt
+```
+### Nota:
+En el punto 5 se dejaron por defecto obtener los primero 1000 items, si se querian bajar mas para llegar a los 5000 por la API el response era **401 authorization required**.
